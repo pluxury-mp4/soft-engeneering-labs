fc17d0da79fcaa20ee99db684144593963a68009
Max Blazhin <m.blazhin@zilant.tech>
2025-03-02 10:09:52 +0300

    add task_3 from laboratory 3-7

---
+++ theme_3/laboratory_work_3-7/task_3.py
@@ -0,0 +1,13 @@
+# Задание 3. Последовательность символов
+for _ in range(6):
+    print("AAA")
+
+for _ in range(5):
+    print("BBBB")
+
+print("E")
+
+for _ in range(9):
+    print("TTTTT")
+
+print("G")
