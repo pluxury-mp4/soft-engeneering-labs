39220a2a9ffe15f031ce0ca1309dcd75ce121693
Max Blazhin <m.blazhin@zilant.tech>
2025-03-17 12:05:11 +0300

    add task_4 from laboratory 4-5

---
+++ theme_4/laboratory_work_4-5/task_4.py
@@ -0,0 +1,6 @@
+# На вход программе подаётся число n и n×n элементов матрицы. Программа выводит максимальный элемент в заштрихованной области.
+
+n = int(input("Введите размер матрицы: "))
+matrix = [list(map(int, input().split())) for _ in range(n)]
+
+print(max(matrix[i][j] for i in range(n) for j in range(i + 1)))
