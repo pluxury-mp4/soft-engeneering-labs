d95d89ebe4aaa6420acc68d86f8f0da0f8a1f7fa
Max Blazhin <m.blazhin@zilant.tech>
2025-03-24 14:28:40 +0300

    add task_1 from laboratory 4-7

---
+++ theme_4/laboratory_work_4-7/task_1.py
@@ -0,0 +1,6 @@
+# Дополните приведенный код, чтобы он вывел элементы множества fruits, каждый на отдельной строке, отсортированные по убыванию (в обратном лексикографическом порядке).
+
+fruits = {'apple', 'banana', 'cherry', 'avocado', 'pineapple', 'apricot', 'banana', 'avocado', 'grapefruit'}
+
+for fruit in sorted(fruits, reverse=True):
+    print(fruit)
